18c6f1a27992109f21c39ae50ce472d9b6d3b8bf
krison11 <54041432+krison11@users.noreply.github.com>
2020-05-07 18:44:32 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -34,14 +34,15 @@ import Vuex from "vuex";
   }
 })
 export default class Index extends Vue {
-head() {
-    return {
-      script: [
-        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
-      ]
-    };
+if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
   }
- 
   // get homePage() {
   //   return this.$store.state.homeContent;
   // }
